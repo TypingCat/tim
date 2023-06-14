0c302b3e1eb4616dfc7bd727d44dcd4f809d279a
Jinhong Noh <Jinhong.P.Noh>
2023-06-14 23:06:58 +0900

    Define TIM more precisely

--- src/infra.cpp
+++ src/infra.cpp
@@ -140,8 +140,7 @@ private:
 	// Communication Protocal Design Specification for Connected Autonomous Driving Service Edge AI v0.5
 	tim::msg::TravelerInformationMessage generate_tim(const std::vector<Object> & observation)
 	{
-		auto tim = tim::msg::TravelerInformationMessage();
-		{
+		auto tim = tim::msg::TravelerInformationMessage(); {
 			auto time = this->get_clock()->now();
 
 			// 1. Message count
@@ -153,30 +152,36 @@ private:
 			traveler_data_frame.set__not_used(0);
 			traveler_data_frame.set__frame_type(1);
 			traveler_data_frame.set__msg_id(0);
-			traveler_data_frame.set__start_time( int(time.seconds()/60)%60 );
-			traveler_data_frame.set__duration_time(1.);
+			traveler_data_frame.set__start_time( int(time.seconds()/60)%60 );	// [minutes]
+			traveler_data_frame.set__duration_time(1.);							// [minutes]
 			traveler_data_frame.set__priority(0);
 
 			traveler_data_frame.set__not_used1(0);
-			traveler_data_frame.set__anchor_lat(pos_x_);
-			traveler_data_frame.set__anchor_long(pos_y_);
+			tim::msg::GeographicalPath region; {
+				region.set__anchor_lat(0);		// [1/10th microdegree]
+				region.set__anchor_long(0);		// [1/10th microdegree]
+			}
+			traveler_data_frame.regions.push_back(region);
 
 			traveler_data_frame.set__not_used2(0);
 			traveler_data_frame.set__not_used3(0);
-			traveler_data_frame.set__advisory(0);
+			traveler_data_frame.set__content(0);
 
 			tim.data_frames.push_back(traveler_data_frame);
 
 			// 3. Regional
-			tim::msg::RegionalExtension regional_extension;
-			{
+			tim::msg::RegionalExtension regional_extension; {
+
 				// 3.1. Edge
-				regional_extension.set__time_stamp( int(time.nanoseconds()/60)%60 );
-				regional_extension.set__processing_time(0);
-				regional_extension.set__edge_id(id_);
-				regional_extension.set__edge_coordinate_system(5186);	// [EPSG]
-				regional_extension.set__edge_x(pos_x_);
-				regional_extension.set__edge_y(pos_y_);
+				regional_extension.set__time_stamp( int(time.nanoseconds()/60)%60 );	// [milliseconds]
+				regional_extension.set__processing_time(0);								// [milliseconds]
+				tim::msg::Edge edge; {
+					edge.set__id(id_);
+					edge.set__coordinate_system(5186);						// EPSG
+					edge.set__x(pos_x_);									// [m]
+					edge.set__y(pos_y_);									// [m]
+				}
+				regional_extension.set__edge(edge);
 				
 				// 3.2. Objects
 				regional_extension.set__num_objects(observation.size());
@@ -184,59 +189,61 @@ private:
 					float x, y, Y;
 					std::vector< std::tuple<float, float> > footprint;
 					std::tie(x, y, Y, footprint) = observed_object.get_states();
+					float dx = x - edge.x;
+					float dy = y - edge.y;
+					float dist = sqrt(pow(dx, 2) + pow(dy, 2));
+					
+					tim::msg::Object object; {
 
-					// 3.2.1. State
-					tim::msg::Object object;
-					object.set__id( observed_object.get_id() );
-					object.set__pose_x( x*100. );					// [m --> cm]
-					object.set__pose_y( y*100. );					// [m --> cm]
-					object.set__pose_angle( Y*180./M_PI/0.0125 );	// [rad --> 0.0125deg]
-					object.set__velocity_x( 0./0.02 );				// [m --> 0.02m]
-					object.set__velocity_y( 0./0.02 );				// [m --> 0.02m]
-					object.set__footprint_num_nodes(4);
-					for (std::tuple<float, float> & node: footprint) {
-						tim::msg::Node n;
-						n.set__x( std::get<0>(node)*100. );			// [m --> cm]
-						n.set__y( std::get<1>(node)*100. );			// [m --> cm]
-						object.footprint_nodes.push_back(n);
-					}
+						// 3.2.1. State					
+						object.set__id( observed_object.get_id() );
+						object.pose.set__x( x*100. );						// [m --> cm]
+						object.pose.set__y( y*100. );						// [m --> cm]
+						object.pose.set__angle( Y*180./M_PI/0.0125 );		// [rad --> 0.0125deg]
+						object.velocity.set__x( 0./0.02 );					// [m --> 0.02m]
+						object.velocity.set__y( 0./0.02 );					// [m --> 0.02m]
 
-					// 3.2.2. Classification
-					object.set__object(tim::msg::Object::OBJECT_CAR);					
-					object.set__object_classification_score(90);
-					object.set__location(tim::msg::Object::LOCATION_CARLANE);
-					object.set__location_classification_score(90);
-					object.set__num_actions(1);
-					tim::msg::Action action;
-					{
-						action.set__action(tim::msg::Action::MOVING);
-						action.set__action_classification_score(90);
-					}
-					object.actions.push_back(action);
-
-					// 3.2.3. Trajectory forecasting
-					object.set__trajectory_forecasting_prediction_horizon(0);
-					object.set__trajectory_forecasting_sampling_period(0);
-					object.set__trajectory_forecasting_num_predictions(0);
-					tim::msg::Prediction prediction;
-					{
-						prediction.set__num_nodes(1);
-						tim::msg::Node node;
-						{
-							node.set__x(object.pose_x);
-							node.set__y(object.pose_y);
+						object.footprint.set__num_nodes(4);
+						for (std::tuple<float, float> & node: footprint) {
+							tim::msg::Node n;
+							n.set__x( (std::get<0>(node) - edge.x)*100. );	// [m --> cm]
+							n.set__y( (std::get<1>(node) - edge.y)*100. );	// [m --> cm]							
+							object.footprint.nodes.push_back(n);
 						}
-						prediction.nodes.push_back(node);
-					}
-					object.trajectory_forecasting_predictions.push_back(prediction);
-					object.set__trajectory_forecasting_score(90);
 
+						// 3.2.2. Classification					
+						object.set__object(	// OBJECT_CAR for detected, OBJECT_UNKNOWN for undetected
+							dist < detection_range_ ? tim::msg::Object::OBJECT_CAR : tim::msg::Object::OBJECT_UNKNOWN);					
+						object.set__object_classification_score(90);
+						object.set__location(tim::msg::Object::LOCATION_CARLANE);
+						object.set__location_classification_score(90);
+						object.set__num_actions(1);
+						tim::msg::Action action; {
+							action.set__action(tim::msg::Action::MOVING);
+							action.set__action_classification_score(90);
+						}
+						object.actions.push_back(action);
+
+						// 3.2.3. Trajectory forecasting
+						object.trajectory_forecasting.set__prediction_horizon(0);
+						object.trajectory_forecasting.set__sampling_period(0);
+						object.trajectory_forecasting.set__num_predictions(1);
+						tim::msg::Prediction prediction; {
+							prediction.set__num_nodes(1);
+							tim::msg::Node node; {
+								node.set__x(object.pose.x);		// [cm]
+								node.set__y(object.pose.y);		// [cm]
+							}
+							prediction.nodes.push_back(node);
+						}
+						object.trajectory_forecasting.predictions.push_back(prediction);
+						object.set__trajectory_forecasting_score(90);
+					}
 					regional_extension.objects.push_back(object);
 				}
 			}
 			tim.regionals.push_back(regional_extension);
 		}
-
 		return tim;
 	}
 
@@ -244,17 +251,15 @@ private:
 	{
 		auto regional = tim.regionals[0];
 		auto tim_visualization = visualization_msgs::msg::MarkerArray();
-		auto coordinate_system = std::to_string(regional.edge_coordinate_system);
-		
-		// 3.1. Edge
 		float edge_height = 5.;
-		visualization_msgs::msg::Marker edge_marker;
-		{
-			edge_marker.header.set__frame_id(coordinate_system);
-			edge_marker.set__id(regional.edge_id);
-			edge_marker.pose.position.set__x(regional.edge_x);	// [m]
-			edge_marker.pose.position.set__y(regional.edge_y);	// [m]
-			edge_marker.pose.position.set__z(edge_height/2.);	// offset[m]
+
+		// 3.1. Edge		
+		visualization_msgs::msg::Marker edge_marker; {
+			edge_marker.header.set__frame_id( std::to_string(regional.edge.coordinate_system) );
+			edge_marker.set__id(regional.edge.id);
+			edge_marker.pose.position.set__x(regional.edge.x);	// [m]
+			edge_marker.pose.position.set__y(regional.edge.y);	// [m]
+			edge_marker.pose.position.set__z(edge_height/2.);	// offset
 			
 			edge_marker.set__ns("edge");
 			edge_marker.set__type(visualization_msgs::msg::Marker::CYLINDER);
@@ -265,27 +270,25 @@ private:
 			edge_marker.color.set__a(0.5);
 			edge_marker.color.set__r(1.);
 			edge_marker.color.set__g(0.);
-			edge_marker.color.set__b(0.);			
+			edge_marker.color.set__b(0.);
 		}
-
 		tim_visualization.markers.push_back(edge_marker);
 
 		// 3.2. Objects
 		for (const auto & object: regional.objects) {
 			visualization_msgs::msg::Marker object_marker;
 
-			object_marker.header.set__frame_id(coordinate_system);
+			object_marker.header.set__frame_id( std::to_string(regional.edge.coordinate_system) );
 			object_marker.set__id(object.id);
-			for (const auto & node: object.footprint_nodes) {
+			for (const auto & node: object.footprint.nodes) {
 				geometry_msgs::msg::Point point;
-				point.set__x( node.x/100. );	// [cm --> m]
-				point.set__y( node.y/100. );	// [cm --> m]
+				point.set__x( node.x/100. + regional.edge.x );						// [cm --> m] & offset
+				point.set__y( node.y/100. + regional.edge.y );						// [cm --> m] & offset
 				object_marker.points.push_back(point);
-			}
-			{
+			} {
 				geometry_msgs::msg::Point point;
-				point.set__x( object.footprint_nodes[0].x/100. );	// [cm --> m]
-				point.set__y( object.footprint_nodes[0].y/100. );	// [cm --> m]
+				point.set__x( object.footprint.nodes[0].x/100. + regional.edge.x );	// [cm --> m] & offset
+				point.set__y( object.footprint.nodes[0].y/100. + regional.edge.y );	// [cm --> m] & offset
 				object_marker.points.push_back(point);
 			}
 			
@@ -293,14 +296,21 @@ private:
 			object_marker.set__type(visualization_msgs::msg::Marker::LINE_STRIP);
 			object_marker.set__action(visualization_msgs::msg::Marker::MODIFY);
 			object_marker.scale.set__x(0.1);
-			object_marker.color.set__a(0.5);
-			object_marker.color.set__r(1.);
-			object_marker.color.set__g(0.);
-			object_marker.color.set__b(0.);	
+			if (object.object == tim::msg::Object::OBJECT_UNKNOWN) {	// BLUE for undetected object
+				object_marker.color.set__a(0.5);
+				object_marker.color.set__r(0.);
+				object_marker.color.set__g(0.);
+				object_marker.color.set__b(1.);
+			}
+			else {														// RED for detected object
+				object_marker.color.set__a(0.5);
+				object_marker.color.set__r(1.);
+				object_marker.color.set__g(0.);
+				object_marker.color.set__b(0.);
+			}			
 
 			tim_visualization.markers.push_back(object_marker);
 		}
-
         return tim_visualization;
 	}
 
@@ -309,9 +319,9 @@ private:
 	rclcpp::TimerBase::SharedPtr timer;
 
 	int id_;
-	float pos_x_{ 0. };
-	float pos_y_{ 0. };
-	float detection_range_{ 100. };
+	float pos_x_{ 10. };			// [m]
+	float pos_y_{ -10. };			// [m]
+	float detection_range_{ 30. };	// [m]
 	int count_{ 0 };
 
 	std::vector<Object> observation_;
